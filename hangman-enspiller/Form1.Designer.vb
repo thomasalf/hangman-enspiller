e437ceb63ed00f5102c70bb84197fb9b0ec7ba8d
thomasalf <thalfh'gmail.com>
2014-10-26 18:10:13 +0100

    Jobbet videre med matrisene

--- hangman-enspiller/Form1.Designer.vb
+++ hangman-enspiller/Form1.Designer.vb
@@ -23,22 +23,33 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Location = New System.Drawing.Point(8, 8)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(144, 23)
+        Me.Button1.Size = New System.Drawing.Size(152, 23)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Start spill med tilfeldig ord"
+        Me.Button1.Text = "Last inn ord i ordlistematrisen"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(8, 40)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(192, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Velg tilfeldig ord og start spill"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(674, 472)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -46,5 +57,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- hangman-enspiller/Form1.vb
+++ hangman-enspiller/Form1.vb
@@ -1,11 +1,34 @@
 ﻿Public Class Form1
 
     Private spillmatrise(9) As String 'Ordet som brukes i denne runden
-    Private ordlistematrise As String 'Alle ordene som kan brukes i spillet
+    Private ordliste(10) As String 'Alle ordene som kan brukes i spillet
+
+
 
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Laster inn ord i ordlistematrisen
+        ordliste(0) = "abbedissen"
+        ordliste(1) = "abdominalt"
+        ordliste(2) = "horekunder"
+        ordliste(3) = "hormonbruk"
+        ordliste(4) = "horisonten"
+        ordliste(5) = "test"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'Sjekker lengden på ordlisten og laster verdien inn i variabel
+        Dim ordlistelengde As Integer
+        ordlistelengde = ordliste.Length
+        MsgBox("ordlistelengde: " & ordlistelengde)
+
+        'Velger tilfeldig tall mellom 1 og ordlistelengde og laster verdien inn i variabel
+        Dim tilfeldig As Integer = CInt(Int((ordlistelengde * Rnd()) + 1))
+
+        'Henter inn ord
 
     End Sub
 End Class
