2e75efd433de2af5bb40e1c7d628659f2a691618
thomasalf <thalfh'gmail.com>
2014-10-26 17:48:00 +0100

    Definerte matrisene

---
+++ hangman-enspiller/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private spillmatrise(9) As String 'Ordet som brukes i denne runden
+    Private ordlistematrise As String 'Alle ordene som kan brukes i spillet
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
